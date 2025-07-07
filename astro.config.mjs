c7f1caf640483e051ec6d0ae9ce4396657355f3f
Joseph Greffen Komala <josephgreffenkomala@mail.ugm.ac.id>
2025-07-07 20:57:32 +0700

    feat: add action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://josephgreffenkomala.github.io',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
